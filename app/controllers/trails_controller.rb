c7c28664e86d4135883070a3960c49733318922f
ackerm44 <ackerm44@gmail.com>
2018-02-15 13:51:35 -0500

    Basic trails index and controller action

--- app/controllers/trails_controller.rb
+++ app/controllers/trails_controller.rb
@@ -1,6 +1,7 @@
 class TrailsController < ApplicationController
 
   def index
+    @trails = Trail.all
   end
 
   def show
@@ -12,7 +13,12 @@ class TrailsController < ApplicationController
   end
 
   def create
-    @trail = Trail.new
+    @trail = Trail.new(trail_params)
+    if @trail.save
+      redirect_to trails_path
+    else
+      render :new
+    end
   end
 
   def edit
@@ -26,7 +32,7 @@ class TrailsController < ApplicationController
 
   private
   def trail_params
-    params.require(:trail).permit(:name, :nearest_city, :directions, :distance, :description, :features, :dog_friendly, region: => [:region_id] )
+    params.require(:trail).permit(:name, :nearest_city, :directions, :distance, :description, :features, :dog_friendly, :region => [:region_id] )
   end
 
 end
--- notes.md
+++ notes.md
@@ -1,5 +1,4 @@
-1.  Set up routes
-1a. Set up basic controllers for each model
+1. Set up basic controllers for each model
 2.  Decide on some basic layouts for each page
 3.  Create new trail nested form
 4.  Work on Devise Authentication
