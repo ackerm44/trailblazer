8058ac735609f2ad15690ceae480efe0f38832d9
ackerm44 <ackerm44@gmail.com>
2018-02-15 11:14:32 -0500

    Migration layout for Trail

--- db/migrate/20180215154146_create_trails.rb
+++ db/migrate/20180215154146_create_trails.rb
@@ -1,7 +1,13 @@
 class CreateTrails < ActiveRecord::Migration[5.1]
   def change
     create_table :trails do |t|
-
+      t.string :name
+      t.string :nearest_city
+      t.string :directions
+      t.float :distance
+      t.text :description
+      t.boolean :dog_friendly
+      t.boolean :user_submitted
       t.timestamps
     end
   end
